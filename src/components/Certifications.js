0bafd83b73961bf486e1056be13f2cdffa64c75d
vaibhav-kadam3107 <99134994+vaibhav-kadam3107@users.noreply.github.com>
2024-07-30 17:49:20 +0530

    Added sytle

--- src/components/Certifications.js
+++ src/components/Certifications.js
@@ -27,7 +27,19 @@ const certifications = [
 
 const Certifications = () => {
   return (
-    <section className="py-16 bg-[rgb(13,15,20)] text-white" id="certifications">
+    <section className="relative py-16 bg-[rgb(13,15,20)] text-white" id="certifications">
+      <div
+        aria-hidden="true"
+        className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+      >
+        <div
+          style={{
+            clipPath:
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+          }}
+          className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-blue-500 to-blue-300 opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+        />
+      </div>
       <div className="container mx-auto px-6 max-w-screen-xl">
         <h2 className="text-4xl font-semibold text-center mb-8">Certifications</h2>
         <div className="w-6 h-1 bg-green-400 md:mx-auto mb-6 md:mb-20"></div>
@@ -49,6 +61,7 @@ const Certifications = () => {
           ))}
         </div>
       </div>
+      
     </section>
   );
 };
